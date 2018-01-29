759beea4995721d890342b1767bf2408a0743c89
Doron Eitan <32599090+doreitan@users.noreply.github.com>
2018-01-29 19:07:54 +0100

    inc.navbar view
    include

---
+++ navbar.blade.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="/">{{config("app.name", "Doro")}}</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="/">Home</a></li>
+            <li><a href="/about">about</a></li>
+            <li><a href="/services">services</a></li>
+            <li><a href="/contact">contact</a></li>
+          </ul>
+        </div>
+      </nav>
